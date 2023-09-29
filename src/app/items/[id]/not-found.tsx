a6be56a7093cbb195f3fa61ba632bc8061007028
slubendo <stephane.lubendo@gmail.com>
2023-09-29 15:15:15 -0700

    database migarted and populated

---
+++ src/app/items/[id]/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div>
+        <h2>404: item doesn't exist</h2>
+      </div>
+    )
+  }
+  
--- src/components/item.tsx
+++ src/components/item.tsx
@@ -1,7 +1,9 @@
-import * as fakeDatabase from "@/fakeDatabase"
 import React from "react"
 import Link from "next/link"
 import Image from "next/image"
+import { db } from "@/db/index"
+import { Item } from "@/db/schema/items"
+
 
 
 interface ItemProps {
@@ -9,22 +11,22 @@ interface ItemProps {
 }
 
 export default function Item({ id }: ItemProps) {
-    const item = fakeDatabase.getItem(id)
 
+    
     return (
         <div className='flex flex-wrap shadow rounded-md'>
             <div className="rounded-full overflow-hidden w-10 h-10">
-                <img className="object-cover object-center w-10 h-10" src={item?.imageUrl ?? "undefined"} alt="" />
+                {/* <img className="object-cover object-center w-10 h-10" src={item?.imageUrl ?? "undefined"} alt="" /> */}
             </div>
 
             <div className="w-4/5">
-                <p>{item?.name}</p>
+                {/* <p>{item?.name}</p>
                 <p>{item?.category}</p>
                 <p>{item?.description}</p>
-                <p>{item?.price}</p>
+                <p>{item?.price}</p> */}
             </div>
 
         </div>
     )
 
-}
+} 
